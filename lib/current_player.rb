27d283d1d00fdd2f26d239a5349fc9bed4e3ef0d
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-12 19:35:22 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,23 +1,19 @@
-# define #turn_count method; accepts argument board array
 def turn_count(board)
-# define counter variable turns, starting at 0 
   turns = 0 
-# iterate board array, assigning move to elements
   board.each do |move|
-# iterate over each space of the board, seeing if any spaces are taken
     if move == "X" || move == "O"
-# increment number of turns by 1 
       turns += 1 
     end 
   end
-# display number of turns
   turns
 end 
 
 # define #current_player method, takes argument of board
 def current_player(board)
+  
 # uses #turn_count to determine who's turn it is 
   turn_count(board)
+  
 # if turn count is even number, return "X", otherwise "O"
 
 
