174c9d8b0a3b26e27efe39605c1a97b507a6b2f9
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-12 19:09:30 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,9 @@
 def turn_count(board)
 # define counter variable turns, starting at 0 
   turns = 0 
-# iterate board array, assigning 
+# iterate board array, assigning move to elements
  board.each do |move|
+# determine how many turns
     if move == "X" || move == "O"
    turns += 1 
      end
