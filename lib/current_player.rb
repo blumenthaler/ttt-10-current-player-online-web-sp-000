fd93ec8872e6d9f2e79b8c46b242a88d106ce960
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-12 19:45:48 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,10 +10,12 @@ end
 
 def current_player(board)
   turns = turn_count(board)
-  if turns % 2 == 0 
-    return "X"
-  else
-    return "O"
-  end 
+  turns % 2 == 0? "X" : "O"
+  
+ # if turns % 2 == 0 
+#    return "X"
+#  else
+#    return "O"
+#  end 
 end 
 
